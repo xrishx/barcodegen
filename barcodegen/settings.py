68b6ccba5a633ebf7fac6f051431dd58fdc78bbb
Rishav Shrestha <69658498+xrishx@users.noreply.github.com>
2025-07-18 15:23:58 +0545

    Prepare for deployment on Render with PostgreSQL

--- barcodegen/settings.py
+++ barcodegen/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,10 +116,9 @@ WSGI_APPLICATION = 'barcodegen.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'  # fallback to SQLite locally
+    )
 }
 
 
