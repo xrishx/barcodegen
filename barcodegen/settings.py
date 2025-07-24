fa88552815d54d13ced3828e7bc7075efb60d215
Rishav Shrestha <69658498+xrishx@users.noreply.github.com>
2025-07-25 01:52:25 +0545

    gl

--- barcodegen/settings.py
+++ barcodegen/settings.py
@@ -146,16 +146,16 @@ WSGI_APPLICATION = 'barcodegen.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
